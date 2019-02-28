d5c504aab14654d49fd9c509502cba4a40c1a0af
Pavel <pav.gre4ixin@yandex.ru>
2019-02-28 11:15:26 +0300

    remove direction subject

--- Source/DirectionSubject.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DirectionSubject.swift
-//  FlowDirection
-//
-//  Created by Pavel Grechikhin on 27/02/2019.
-//  Copyright © 2019 pavel.grechikhin. All rights reserved.
-//
-
-import Foundation
